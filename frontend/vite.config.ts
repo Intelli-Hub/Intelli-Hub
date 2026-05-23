4eefb6546733917be7026653ead751e803c8f1c9
anshiiijain <137139041+anshiiijain@users.noreply.github.com>
2026-05-24 02:20:03 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-// https://vite.dev/config/
-
 import tsconfigPaths from 'vite-plugin-tsconfig-paths'
 
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+  ],
 })
