b9be3dc0199c628fc3738965da0da27f8a84b293
Raushan-Shahi <raushanshahi2001@gmail.com>
2026-05-27 00:37:42 +0530

    feat: add Prisma client and test DB connection

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,13 +1,15 @@
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis;
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-  });
+const prisma = globalThis.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+  globalThis.prisma = prisma;
 }
+
+export default prisma;
